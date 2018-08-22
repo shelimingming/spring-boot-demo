6df9d0175284ba9c229a4b2b807a300586337f0a
shelimingming <shelimingming@qq.com>
2018-08-23 00:03:40 +0800

    spring-boot-admin-eureka

---
+++ spring-boot-admin-eureka/spring-boot-admin-server/src/main/java/com/sheliming/admin/eureka/server/AdminServerApplication.java
@@ -0,0 +1,17 @@
+package com.sheliming.admin.eureka.server;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableAdminServer
+public class AdminServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+}
