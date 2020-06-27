0647ed15bb5256672807ba89534754a85b69cb1a
sheliming <shelimingming@qq.com>
2020-06-27 23:27:57 +0800

    修改一些小问题

--- spring-boot-helloworld/src/main/java/com/sheliming/App.java
+++ spring-boot-helloworld/src/main/java/com/sheliming/App.java
@@ -3,7 +3,7 @@ package com.sheliming;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication  //等同于@EnableAutoConfiguration+ @ComponentScan("com.sheliming")
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
