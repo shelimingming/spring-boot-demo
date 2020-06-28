0dee15b8305c50832c45503e05aed67952c4afeb
sheliming <shelimingming@qq.com>
2020-06-29 00:16:19 +0800

    自定义starter

---
+++ demo-spring-boot-starter/src/main/java/com/sheliming/config/DemoConfig.java
@@ -0,0 +1,26 @@
+package com.sheliming.config;
+
+import com.sheliming.properties.DemoProperties;
+import com.sheliming.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(DemoProperties.class)
+@ConditionalOnProperty(
+        prefix = "demo",
+        name = "isopen",
+        havingValue = "true"
+)
+public class DemoConfig {
+    @Autowired
+    private DemoProperties demoProperties;
+
+    @Bean(name = "demo")
+    public DemoService demoService(){
+        return new DemoService(demoProperties.getSayWhat(), demoProperties.getToWho());
+    }
+}
---
+++ demo-spring-boot-starter/src/main/java/com/sheliming/properties/DemoProperties.java
@@ -0,0 +1,25 @@
+package com.sheliming.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "demo")
+public class DemoProperties {
+    private String sayWhat;
+    private String toWho;
+
+    public String getSayWhat() {
+        return sayWhat;
+    }
+
+    public void setSayWhat(String sayWhat) {
+        this.sayWhat = sayWhat;
+    }
+
+    public String getToWho() {
+        return toWho;
+    }
+
+    public void setToWho(String toWho) {
+        this.toWho = toWho;
+    }
+}
