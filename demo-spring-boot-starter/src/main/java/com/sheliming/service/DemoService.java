47a39725d460a4d00254c0f2fe9d1ad1801d7f2d
sheliming <shelimingming@qq.com>
2020-06-29 23:33:19 +0800

    自定义starter

--- demo-spring-boot-starter/src/main/java/com/sheliming/service/DemoService.java
+++ demo-spring-boot-starter/src/main/java/com/sheliming/service/DemoService.java
@@ -3,11 +3,17 @@ package com.sheliming.service;
 public class DemoService {
     public String sayWhat;
     public String toWho;
-    public DemoService(String sayWhat, String toWho){
+
+    public DemoService() {
+
+    }
+
+    public DemoService(String sayWhat, String toWho) {
         this.sayWhat = sayWhat;
         this.toWho = toWho;
     }
-    public String say(){
+
+    public String say() {
         return this.sayWhat + "!  " + toWho;
     }
 }
